4eabb6f5cba32a4a3d5850c57e5ab1e5d074a196
Isadora Goedert☆ <99146463+isagrt@users.noreply.github.com>
2025-04-17 16:14:30 -0300

    Add files via upload

---
+++ projeto1.c
@@ -0,0 +1,23 @@
+
+
+//1.Peça um caractere e mostre o código w mostre o código ASCII correspondente.
+//2. Peça duas letras e diga qual vem primeiro no alfabeto.
+//3. Peça uma letra e diga se ela é maiúscula ou minúscula.
+//4. Inverta a letra digitada: se fr msiuscula transforma em maiuscula e se for miniscula tranforma em maiuscula.
+
+
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+  char letter;
+
+  printf("Digite uma letra: ");
+  scanf("%c", &letter); //%d/n
+
+  printf("O código ASCII da letra %c é : %d \n",letter, letter);
+  
+
+ 
+}
+
