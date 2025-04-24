462af1b765fed2c3a9a99b1ce83484cb58061189
Isadora Goedert☆ <99146463+isagrt@users.noreply.github.com>
2025-04-24 16:33:12 -0300

    Projeto no qual peça uma letra e diga se ela é maiúscula ou minúscula.

--- projeto3.c
+++ projeto3.c
@@ -1,5 +1,3 @@
-
-
 //1.Peça um caractere e mostre o código w mostre o código ASCII correspondente.
 //2. Peça duas letras e diga qual vem primeiro no alfabeto.
 //3. Peça uma letra e diga se ela é maiúscula ou minúscula.
